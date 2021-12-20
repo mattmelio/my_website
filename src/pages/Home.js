9fe651e7b13afedfa668deaf88e8168e6e3c6258
MattMelio <matthew.melio@yahoo.com>
2021-12-20 01:20:15 -0500

    home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
 function Home() {
     return(
+        <>
         <div>Home. will add threejs tomorrow</div>
+        <h1>Click <a href ="https://github.com/mattmelio/my_website" target="_blank">here</a> for link to my github repository with all website code</h1>
+        <br/><br/>
+        <p>This sites creation date is December 20th, 2021</p>
+        </>
     );
 }
 
